8e260366ac4e9e6d71c7848e77ef274f842cbabf
KajdeMunter <kajdemunter@hotmail.com>
2018-12-11 13:08:46 +0100

    Added font awesome

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,21 @@ import App from './App.vue';
 import { router } from './router';
 import store from './_store';
 import './registerServiceWorker';
+
+// Font awesome
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCoffee, faMapMarkerAlt, faCog, faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
 library.add(faCoffee, faMapMarkerAlt, faCog, faBars);
-
 Vue.component('font-awesome-icon', FontAwesomeIcon);
-
-Vue.config.productionTip = false;
 
 // setup fake backend
 import { configureFakeBackend } from '@/_helpers/fake-backend';
 configureFakeBackend();
 
+Vue.config.productionTip = false;
+
+// Create vue app
 new Vue({
   router,
   store,
