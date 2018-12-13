ae065956990e55e4520c4e86d89413323ba2e1cb
Jor180 <0920140@hr.nl>
2018-12-13 13:40:56 +0100

    Styled settings page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <font-awesome-icon icon="cog" class="hdr"></font-awesome-icon>
         <h1>Settings</h1>
         <br>
         <img src="../assets/Avatar.png" alt="Avatar" class="avatar">
@@ -8,7 +9,10 @@
         <p>Account information</p>
         <p>User Agreement</p>
         <p>Report a bug</p>
-        <p>
+        <br>
+        <p class="link">Build version 0.01</p>
+        <p class="link">Open Source Licences</p>
+        <p class="unq">
             <router-link to="/login" class="btn btn-danger">Logout</router-link>
         </p>
     </div>
@@ -27,6 +31,10 @@
         position: relative;
     }
 
+    h2 {
+        color: #ffffff;
+    }
+
     .avatar {
         vertical-align: middle;
         width: 100px;
@@ -40,7 +48,39 @@
     p {
         color: #999999;
         background: #ffffff;
-        background-size: 100%;
+        background-size: 50%;
+        alignment: left;
+        padding: 20px;
+        margin: 20px;
+    }
+
+    .hdr {
+        color: #ffffff;
+        font-size: 2em;
+        position: relative;
+        right: 20px;
+    }
+
+    .btn {
+        color: #ffffff;
+        padding: 10px;
+        margin: 5px;
+        background: #7e838c;
+        border-color: #006633;
+
+    }
+
+    .link{
+        background: #006633;
+        color: #7e838c;
+        padding: 5px;
+        border: 5px #006633 solid;
+
+    }
+
+    .unq {
+        background: #006633;
+
     }
 
 </style>
