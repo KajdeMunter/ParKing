d40fc1e6e3cbf1d5e06bedcf22c13875451bade7
Jor180 <0920140@hr.nl>
2018-12-13 11:22:19 +0100

    removed broken

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,23 +3,11 @@
         <h1>Settings</h1>
         <br>
         <img src="../assets/Avatar.png" alt="Avatar" class="avatar">
-        <h2>Hi {{account.user.firstName}}!</h2>
+        <h2>Hi name!</h2>
         <p>example@email100.com</p>
         <p>Account information</p>
         <p>User Agreement</p>
         <p>Report a bug</p>
-
-
-        <em v-if="users.loading">Loading users...</em>
-        <span v-if="users.error" class="text-danger">ERROR: {{users.error}}</span>
-        <ul v-if="users.items">
-            <li v-for="user in users.items" :key="user.id">
-                {{user.firstName + ' ' + user.lastName}}
-                <span v-if="user.deleting"><em> - Deleting...</em></span>
-                <span v-else-if="user.deleteError" class="text-danger"> - ERROR: {{user.deleteError}}</span>
-                <span v-else> - <a @click="deleteUser(user.id)" class="text-danger">Delete</a></span>
-            </li>
-        </ul>
         <p>
             <router-link to="/login" class="btn btn-danger">Logout</router-link>
         </p>
@@ -27,28 +15,9 @@
 </template>
 
 <script>
-    import {mapState, mapActions} from 'vuex'
-
     export default {
         name: "Settings",
-        computed: {
-            ...mapState({
-                account: state => state.account,
-                users: state => state.users.all
-            })
-        },
-        created() {
-            this.getAllUsers();
-        },
-        methods: {
-            ...mapActions('users', {
-                getAllUsers: 'getAll',
-                deleteUser: 'delete'
-            })
-        }
     };
-
-
 </script>
 
 <style lang="scss">
